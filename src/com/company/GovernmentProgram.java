88d28d750c7064619c5dfc5e87cd27acae4db312
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-30 11:18:03 -0400

    Method Overloading

--- src/com/company/GovernmentProgram.java
+++ src/com/company/GovernmentProgram.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Random;
+
 public class GovernmentProgram {
+    private String programName;
+
+    public GovernmentProgram(){
+        String[] programList = {"Medicare", "Medicade", "VA", "ObamaCare", "CongressCare"};
+        var picker = new Random();
+        var number = picker.nextInt(programList.length);
+        programName = programList[number];
+    }
 
     public void payBill(double cost){
-
+        if (programName.equals("CongressCare"))
+            System.out.println("Congress Care is paying a bill of $" + cost + " and giving a kickback ... um tip of $" + cost+2);
+        else if (programName.equals("Medicare"))
+            System.out.println("Medicare is paying $" + cost + ". Welcome senior citizen.");
+        else
+            System.out.println(programName + " is paying $" + cost + ".");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Person sickPerson1 = new Person("Stu Dent");
+        Person sickPerson2 = new Person ("Any Body");
+        var govProg = new GovernmentProgram();
         Doctor dr = new Doctor(300, "Expensive U");
+
         dr.treatPatient(sickPerson1);
         dr.billPatient(sickPerson1);
+
+        dr.treatPatient(sickPerson2);
+        dr.billPatient(false, govProg);
     }
 }
